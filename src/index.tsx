d12551ecc395510999499fd9028bf03bce79914a
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2023-10-30 15:13:34 -0700

    Fix a few errors.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function App() {
+export function App() {
 	return (
 		<div className='w-screen h-screen flex flex-col justify-center items-center shadow-xl bg-ctp-crust'>
-			<h1 className='font-black text-5xl lg:text-6xl mb-4 bg-gradient-to-r bg-clip-text text-transparent from-ctp-green to-ctp-blue p-6'>
+			<h1 className='font-black text-5xl lg:text-6xl mb-4 bg-gradient-to-r bg-clip-text text-transparent from-ctp-red to-ctp-blue p-6'>
 				Homepage
 			</h1>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
