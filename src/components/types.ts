ec18235d953b75b66390440a709b881089d107e9
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2023-12-04 09:30:09 -0800

    Add PWA support and update tailwind.config.js

---
+++ src/components/types.ts
@@ -0,0 +1,9 @@
+declare module 'virtual:pwa-register' {
+	import type { RegisterSWOptions } from 'vite-plugin-pwa/types';
+
+	export type { RegisterSWOptions };
+
+	export function registerSW(
+		options?: RegisterSWOptions,
+	): (reloadPage?: boolean) => Promise<void>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaConfig: Partial<VitePWAOptions> = {
+	registerType: 'autoUpdate',
+	includeAssets: ['*'],
+	manifest: {
+		short_name: 'Homepage',
+		name: 'New Tab',
+		icons: [
+			{
+				src: '/icon.png',
+				type: 'image/png',
+				sizes: '192x192',
+			},
+			{
+				src: '/icon.png',
+				type: 'image/png',
+				sizes: '512x512',
+			},
+			{
+				src: '/maskable_icon.png',
+				type: 'image/png',
+				sizes: '418x418',
+				purpose: 'any maskable',
+			},
+		],
+		id: '/?source=pwa',
+		start_url: '/?source=pwa',
+		background_color: '#1e1e2e',
+		display: 'standalone',
+		scope: '/',
+		theme_color: '#1e1e2e',
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), VitePWA(pwaConfig)],
 	server: {
 		watch: {
 			usePolling: true,
