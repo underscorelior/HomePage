0b082261161abe3501d84bebe4d0c28a75087f17
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2024-01-05 02:20:11 -0800

    Clean up types

--- src/components/types.ts
+++ src/components/types.ts
@@ -8,55 +8,10 @@ declare module 'virtual:pwa-register' {
 	): (reloadPage?: boolean) => Promise<void>;
 }
 
-interface UserProfile {
-	country: string;
-	display_name: string;
-	email: string;
-	explicit_content: {
-		filter_enabled: boolean;
-		filter_locked: boolean;
-	};
-	external_urls: { spotify: string };
-	followers: { href: string; total: number };
-	href: string;
-	id: string;
-	images: Image[];
-	product: string;
-	type: string;
-	uri: string;
-}
-
 interface Image {
 	url: string;
 	height: number;
 	width: number;
-}
-
-interface CurrentlyPlaying {
-	device: {
-		id: string | null;
-		is_active: boolean;
-		is_private_session: boolean;
-		is_restricted: boolean;
-		name: string;
-		type: string;
-		volume_percent: number | null;
-		supports_volume: boolean;
-		repeat_state: string;
-		shuffle_state: boolean;
-	};
-	context: {
-		type: string;
-		href: string;
-		external_urls: {
-			spotify: string;
-		};
-		uri: string;
-	} | null;
-	timestamp: number;
-	progress_ms: number;
-	is_playing: boolean;
-	item: TrackObject;
 }
 
 interface TrackObject {
@@ -96,14 +51,4 @@ interface TrackObject {
 	type: 'track';
 	uri: string;
 	is_local: boolean;
-}
-
-interface Device {
-	id: string;
-	is_active: boolean;
-	is_private_session: boolean;
-	is_restricted: boolean;
-	name: string;
-	type: string;
-	volume_percent: number;
 }
