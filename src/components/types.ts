d14f822be2541596cd0d0662f078eed868c7d218
underscorelior <newlioryt@gmail.com>
2024-01-06 01:58:55 -0800

    Fix types

--- src/components/types.ts
+++ src/components/types.ts
@@ -100,3 +100,13 @@ interface WeatherApiResponse {
 	name: string;
 	cod: number;
 }
+
+interface Device {
+	id: string;
+	is_active: boolean;
+	is_private_session: boolean;
+	is_restricted: boolean;
+	name: string;
+	type: string;
+	volume_percent: number;
+}
