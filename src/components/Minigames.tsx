58269fd70aa11d2197e9a8e90569d61de56f9a72
underscorelior <newlioryt@gmail.com>
2024-01-09 23:31:58 -0800

    Minigames

---
+++ src/components/Minigames.tsx
@@ -0,0 +1,13 @@
+import { FaGamepad } from 'react-icons/fa6';
+
+function Minigames() {
+	return (
+		<div className="fixed bottom-0 left-0 m-3 w-full">
+			<button className="bg-ctp-crust ring-ctp-surface0 flex h-14 w-14 items-center justify-center rounded-lg p-4 ring">
+				<FaGamepad className="fill-ctp-sky h-10 w-10" size={'4em'} />
+			</button>
+		</div>
+	);
+}
+
+export default Minigames;
