75fb1fea7f58e407c61dd3fa65b409bb27e3c00b
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2024-01-10 17:49:39 -0800

    Update Minigames component

--- src/components/Minigames.tsx
+++ src/components/Minigames.tsx
@@ -2,7 +2,7 @@ import { FaGamepad } from 'react-icons/fa6';
 
 function Minigames() {
 	return (
-		<div className="fixed bottom-0 left-0 m-3 w-full">
+		<div className="fixed bottom-0 left-0 m-4 w-full">
 			<button className="bg-ctp-crust ring-ctp-surface0 flex h-14 w-14 items-center justify-center rounded-lg p-4 ring">
 				<FaGamepad className="fill-ctp-sky h-10 w-10" size={'4em'} />
 			</button>
