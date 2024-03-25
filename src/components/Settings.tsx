2ec23d711e4b7337333477e339ba3764fd050583
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2024-03-25 00:21:06 -0700

    Fix vercel not working

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,5 +1,5 @@
 // TODO: Switch to cookies
-
+// FIX HOVER FOR SPOTIFY AND FIX GAMRPAD COLOR
 import { useEffect, useState } from 'react';
 
 import {
@@ -11,7 +11,7 @@ import { ToggleGroup, ToggleGroupItem } from '@/shadcn/components/toggle-group';
 import {
 	AlertDialog,
 	AlertDialogAction,
-	AlertDialogCancel,
+	// AlertDialogCancel,
 	AlertDialogContent,
 	AlertDialogDescription,
 	AlertDialogFooter,
@@ -23,7 +23,7 @@ import { Input } from '@/shadcn/components/ui/input';
 
 import { FiSettings } from 'react-icons/fi';
 import { SiSpotify } from 'react-icons/si';
-import { FaTasks } from 'react-icons/fa';
+// import { FaTasks } from 'react-icons/fa';
 import { FaGamepad } from 'react-icons/fa6';
 import { TiWeatherCloudy } from 'react-icons/ti';
 
@@ -32,32 +32,30 @@ function ApiKeyPopup() {
 
 	return (
 		<AlertDialog>
-			<AlertDialogTrigger>
-				<button className=" text-ctp-text mt-4 rounded-lg border-2 border-neutral-700 p-3 font-semibold">
-					API Keys
-				</button>
+			<AlertDialogTrigger className="mx-auto mt-4 w-max rounded-lg border-2 border-neutral-700 p-3 font-semibold text-neutral-300 outline-none">
+				API Keys
 			</AlertDialogTrigger>
-			<AlertDialogContent className="bg-ctp-crust border-ctp-surface1 border-2">
+			<AlertDialogContent className="border-2 border-neutral-700 bg-neutral-950">
 				<AlertDialogHeader>
-					<AlertDialogTitle className=" text-ctp text pb-3 text-center text-3xl font-bold tracking-wide">
+					<AlertDialogTitle className="pb-3 text-center text-3xl font-bold tracking-wide text-neutral-300">
 						API Keys
 					</AlertDialogTitle>
 					<AlertDialogDescription>
 						<div className="flex flex-col gap-y-4">
 							<div className="flex flex-row gap-x-4">
-								<TiWeatherCloudy className=" text-ctp-sky h-10 w-10" />
+								<TiWeatherCloudy className="h-10 w-10 text-orange-300" />
 								<Input
 									placeholder="OpenWeatherMap API Key"
 									type="password"
-									className="bg-ctp-crust text-ctp text placeholder-ctp-surface2 focus:-outline-ctp-overlay2 focus:outline-ctp-surface2 active:-outline-ctp-surface2 w-full rounded-lg border-neutral-700 px-3 text-lg outline-none ring-2 ring-neutral-700 placeholder:font-bold"
+									className="w-full rounded-lg border-neutral-700 bg-neutral-950 px-3 text-lg text-neutral-400 placeholder-stone-200 outline-none ring-2 ring-neutral-700 placeholder:font-bold focus:outline-stone-800 active:outline-stone-800"
 								/>
 							</div>
 							<div className="flex flex-row gap-x-4">
-								<SiSpotify className=" text-ctp-green h-10 w-10" />
+								<SiSpotify className="h-10 w-10 text-green-400" />
 								<Input
 									placeholder="Spotify Client ID"
 									type="password"
-									className="bg-ctp-crust  text-ctp text placeholder-ctp-surface2 focus:-outline-ctp-overlay2 focus:outline-ctp-surface2 active:-outline-ctp-surface2 w-full rounded-lg border-neutral-700 px-3 text-lg outline-none ring-2 ring-neutral-700 placeholder:font-bold"
+									className="w-full rounded-lg border-neutral-700 bg-neutral-950 px-3 text-lg text-neutral-400 placeholder-stone-200 outline-none ring-2 ring-neutral-700 placeholder:font-bold focus:outline-stone-800 active:outline-stone-800"
 								/>
 							</div>
 						</div>
@@ -66,7 +64,7 @@ function ApiKeyPopup() {
 				<AlertDialogFooter>
 					{/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
 					<AlertDialogAction
-						className="bg-ctp-crust border-2 border-neutral-700"
+						className="border-2 border-neutral-700 bg-neutral-950"
 						onClick={() => saveKeys()}>
 						Save
 					</AlertDialogAction>
@@ -78,11 +76,11 @@ function ApiKeyPopup() {
 
 export default function Settings({
 	setSpotify,
-	setTasks,
+	// setTasks,
 	setMinigames,
 }: {
 	setSpotify: (s: boolean) => void;
-	setTasks: (s: boolean) => void;
+	// setTasks: (s: boolean) => void;
 	setMinigames: (s: boolean) => void;
 	setSettingsOpen: (s: boolean) => void;
 	settingsOpen: boolean;
@@ -96,8 +94,8 @@ export default function Settings({
 		[values, change] = handleCheck('spotify', values);
 		setSpotify(change);
 
-		[values, change] = handleCheck('tasks', values);
-		setTasks(change);
+		// [values, change] = handleCheck('tasks', values);
+		// setTasks(change);
 
 		[values, change] = handleCheck('minigames', values);
 		setMinigames(change);
@@ -129,7 +127,7 @@ export default function Settings({
 
 		setToggleVal(s);
 		setSpotify(s.includes('spotify'));
-		setTasks(s.includes('tasks'));
+		// setTasks(s.includes('tasks'));
 		setMinigames(s.includes('minigames'));
 	}
 
@@ -144,11 +142,11 @@ export default function Settings({
 	return (
 		<Popover>
 			<PopoverTrigger className="m-4 mr-2 w-full" onClick={() => checkValues()}>
-				<div className="bg-ctp-crust hover:bg-ctp-base flex h-14 w-14 items-center justify-center rounded-lg p-4 ring-[3px] ring-neutral-700 transition-all duration-200 ease-in-out">
-					<FiSettings className=" text-ctp-sky h-10 w-10" size={'4em'} />
+				<div className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-950 p-4 ring-2 ring-neutral-800 transition-all duration-200 ease-in-out hover:bg-neutral-900">
+					<FiSettings className="h-10 w-10 text-stone-300" size={'4em'} />
 				</div>
 			</PopoverTrigger>
-			<PopoverContent className="bg-ctp-crust mb-1 ml-2 flex w-full flex-col rounded-lg border-[3px] border-neutral-700 p-4">
+			<PopoverContent className="mb-1 ml-2 flex w-full flex-col rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
 				<ToggleGroup
 					type="multiple"
 					className="space-x-2"
@@ -156,30 +154,30 @@ export default function Settings({
 					onValueChange={(value) => toggleValues(value)}>
 					<ToggleGroupItem
 						value="spotify"
-						className="hover:bg-ctp-mantle data-[state=on]:bg-ctp-base flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3">
-						<SiSpotify className=" text-ctp-sapphire h-7 w-7" />
+						className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-green-800/70 hover:bg-neutral-800 hover:text-green-400 data-[state=on]:bg-neutral-900 data-[state=on]:text-green-500">
+						<SiSpotify className="h-7 w-7" />
 					</ToggleGroupItem>
-					<ToggleGroupItem
+					{/* <ToggleGroupItem
 						value="tasks"
-						className="hover:bg-ctp-mantle data-[state=on]:bg-ctp-base flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3">
+						className="hover:bg-neutral-800 data-[state=on]:bg-neutral-900 flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3">
 						<FaTasks className=" text-ctp-sapphire h-7 w-7" />
-					</ToggleGroupItem>
+					</ToggleGroupItem> */}
 					<ToggleGroupItem
 						value="minigames"
-						className="hover:bg-ctp-mantle data-[state=on]:bg-ctp-base flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3">
-						<FaGamepad className=" text-ctp-sapphire h-7 w-7" />
+						className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-red-800/70 hover:bg-neutral-800 hover:text-red-400 data-[state=on]:bg-neutral-900 data-[state=on]:text-red-600 data-[state=on]:hover:bg-neutral-800 data-[state=on]:hover:text-red-500">
+						<FaGamepad className="h-7 w-7" />
 					</ToggleGroupItem>
 				</ToggleGroup>
 				<button
 					onClick={() => resetSpotifyData()}
-					className=" text-ctp-text mt-4 flex flex-col rounded-lg border-2 border-neutral-700 p-3 font-semibold">
+					className="mt-4 flex flex-col rounded-lg border-2 border-neutral-700 p-3 font-semibold text-neutral-300">
 					Reset Spotify Data{' '}
-					<span className=" text-ctp-subtext0 text-xs font-medium">
+					<span className="text-xs font-medium text-neutral-400">
 						(Use if the spotify widget isn't loading.)
 					</span>
 				</button>
 				<ApiKeyPopup />
-				{/* <span className=" text-ctp-subtext0 pt-4 text-xs font-medium">
+				{/* <span className=" text-neutral-400 pt-4 text-xs font-medium">
 					(Press the "s" key to toggle the settings button.)
 				</span> */}
 			</PopoverContent>
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import './index.css';
 import Clock from './components/Clock';
 import Spotify from './components/Spotify';
 import { Toaster } from 'react-hot-toast';
-import Tasks from './components/Tasks';
-import Settings from './components/Settings';
-import Minigames from './components/Minigames';
-import { useState } from 'react';
+// import Tasks from './components/Tasks';
+// import Settings from './components/Settings';
+// import Minigames from './components/Minigames';
+// import { useState } from 'react';
 
 export function App() {
-	const [spotify, setSpotify] = useState<boolean>(false);
-	const [tasks, setTasks] = useState<boolean>(false);
-	const [minigames, setMinigames] = useState<boolean>(false);
-	const [settingsOpen, setSettingsOpen] = useState<boolean>(true);
+	// const [spotify, setSpotify] = useState<boolean>(false);
+	// const [tasks, setTasks] = useState<boolean>(false);
+	// const [minigames, setMinigames] = useState<boolean>(false);
+	// const [settingsOpen, setSettingsOpen] = useState<boolean>(true);
 
 	return (
 		<div className="flex h-screen w-screen flex-col overflow-hidden bg-neutral-950">
@@ -20,17 +20,18 @@ export function App() {
 			<Clock />
 			<div className="fixed bottom-0 flex h-max w-full justify-center">
 				<div className="fixed bottom-0 left-0 flex flex-row">
-					<Settings
+					{/* <div className="fixed bottom-0 left-0 flex flex-row">
 						setSpotify={setSpotify}
-						setTasks={setTasks}
+						// setTasks={setTasks}
 						setMinigames={setMinigames}
 						setSettingsOpen={setSettingsOpen}
 						settingsOpen={settingsOpen}
 					/>
 					{minigames && <Minigames />}
+				</div> */}
+					<Spotify />
+					{/* {tasks && <Tasks />} */}
 				</div>
-				{spotify && <Spotify />}
-				{tasks && <Tasks />}
 			</div>
 		</div>
 	);
