eead71f09e685f7ed1db4ba5aaeda6f6f5a307c7
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2024-04-05 00:04:13 -0700

    Add a NYI toast error

--- src/components/Minigames.tsx
+++ src/components/Minigames.tsx
@@ -1,10 +1,26 @@
+import toast from 'react-hot-toast';
 import { FaGamepad } from 'react-icons/fa6';
 
 function Minigames() {
 	return (
 		<div className="m-4 ml-2 w-full">
-			<button className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-900 p-4 ring ring-neutral-700">
-				<FaGamepad className="fill-ctp-sky h-10 w-10" size={'4em'} />
+			<button
+				className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-950 p-4 ring-2 ring-neutral-800 transition-all duration-200 ease-in-out hover:bg-neutral-900"
+				onClick={() =>
+					toast.error('NYI!', {
+						style: {
+							border: '1px solid #262626',
+							padding: '16px',
+							color: '#d4d4d4',
+							backgroundColor: '#0a0a0a',
+						},
+						iconTheme: {
+							primary: '#dc2626',
+							secondary: '#171717',
+						},
+					})
+				}>
+				<FaGamepad className="h-10 w-10 fill-stone-300" size={'4em'} />
 			</button>
 		</div>
 	);
