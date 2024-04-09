4d305efa5014b44d75e3f0eddca7b6ae56d1100b
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2024-04-09 00:57:04 -0700

    Massively overhaul Settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,17 +1,9 @@
-// TODO: Switch to cookies
-// FIX HOVER FOR SPOTIFY AND FIX GAMRPAD COLOR
 import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from '@/shadcn/components/popover';
-import { ToggleGroup, ToggleGroupItem } from '@/shadcn/components/toggle-group';
 import {
 	AlertDialog,
 	AlertDialogAction,
-	// AlertDialogCancel,
 	AlertDialogContent,
 	AlertDialogDescription,
 	AlertDialogFooter,
@@ -19,13 +11,30 @@ import {
 	AlertDialogTitle,
 	AlertDialogTrigger,
 } from '@/shadcn/components/alert-dialog';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/shadcn/components/popover';
+import {
+	ContextMenu,
+	ContextMenuContent,
+	ContextMenuTrigger,
+} from '@/shadcn/components/ui/context-menu';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/shadcn/components/tooltip';
+
+import { ToggleGroup, ToggleGroupItem } from '@/shadcn/components/toggle-group';
 import { Input } from '@/shadcn/components/ui/input';
 
-import { FiSettings } from 'react-icons/fi';
-import { SiSpotify } from 'react-icons/si';
-// import { FaTasks } from 'react-icons/fa';
-import { FaGamepad } from 'react-icons/fa6';
+import { FiCalendar, FiSettings } from 'react-icons/fi';
+import { TbTemperatureCelsius, TbTemperatureFahrenheit } from 'react-icons/tb';
 import { TiWeatherCloudy } from 'react-icons/ti';
+import { SiSpotify } from 'react-icons/si';
 
 function ApiKeyPopup() {
 	function saveKeys() {}
@@ -73,19 +82,20 @@ function ApiKeyPopup() {
 		</AlertDialog>
 	);
 }
-
 export default function Settings({
 	setSpotify,
-	// setTasks,
-	setMinigames,
+	setTemp,
+	setCountdown,
+	setWeather,
 }: {
 	setSpotify: (s: boolean) => void;
-	// setTasks: (s: boolean) => void;
-	setMinigames: (s: boolean) => void;
-	setSettingsOpen: (s: boolean) => void;
-	settingsOpen: boolean;
+	setTemp: (s: string) => void;
+	setCountdown: (s: boolean) => void;
+	setWeather: (s: boolean) => void;
 }) {
 	const [toggleVal, setToggleVal] = useState<string[]>([]);
+
+	// WTF IS GOING ON HERE?
 
 	function checkValues() {
 		let values: string[] = [];
@@ -94,11 +104,11 @@ export default function Settings({
 		[values, change] = handleCheck('spotify', values);
 		setSpotify(change);
 
-		// [values, change] = handleCheck('tasks', values);
-		// setTasks(change);
+		[values, change] = handleCheck('countdown', values);
+		setCountdown(change);
 
-		[values, change] = handleCheck('minigames', values);
-		setMinigames(change);
+		[values, change] = handleCheck('weather', values);
+		setWeather(change);
 
 		setToggleVal(values);
 		return values;
@@ -122,13 +132,24 @@ export default function Settings({
 
 	function toggleValues(s: string[]) {
 		localStorage.setItem('is_spotify', s.includes('spotify').toString());
-		localStorage.setItem('is_tasks', s.includes('tasks').toString());
-		localStorage.setItem('is_minigames', s.includes('minigames').toString());
 
 		setToggleVal(s);
 		setSpotify(s.includes('spotify'));
-		// setTasks(s.includes('tasks'));
-		setMinigames(s.includes('minigames'));
+
+		localStorage.setItem('is_countdown', s.includes('countdown').toString());
+
+		setToggleVal(s);
+		setCountdown(s.includes('countdown'));
+
+		localStorage.setItem('is_weather', s.includes('weather').toString());
+
+		setToggleVal(s);
+		setWeather(s.includes('weather'));
+	}
+
+	function toggleTemp(s: string) {
+		localStorage.setItem('temp', s);
+		setTemp(s);
 	}
 
 	useEffect(() => {
@@ -136,51 +157,117 @@ export default function Settings({
 	}, []);
 
 	function resetSpotifyData(): void {
-		throw new Error('Function not implemented.');
+		localStorage.removeItem('spotify_verifier');
+		localStorage.removeItem('spotify_access_token');
+		localStorage.removeItem('spotify_refresh_token');
+		localStorage.removeItem('spotify_expires_in');
+		toast.success(`Successfully cleared spotify authentication data.`, {
+			style: {
+				border: '1px solid #171717',
+				padding: '12px',
+				color: '#d4d4d4',
+				backgroundColor: '#0a0a0a',
+				textAlign: 'center',
+			},
+			iconTheme: {
+				primary: '#16a34a',
+				secondary: '#171717',
+			},
+		});
 	}
 
 	return (
-		<Popover>
-			<PopoverTrigger className="m-4 mr-2 w-full" onClick={() => checkValues()}>
-				<div className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-950 p-4 ring-2 ring-neutral-800 transition-all duration-200 ease-in-out hover:bg-neutral-900">
-					<FiSettings className="h-10 w-10 text-stone-300" size={'4em'} />
-				</div>
-			</PopoverTrigger>
-			<PopoverContent className="mb-1 ml-2 flex w-full flex-col rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
-				<ToggleGroup
-					type="multiple"
-					className="space-x-2"
-					defaultValue={toggleVal}
-					onValueChange={(value) => toggleValues(value)}>
-					<ToggleGroupItem
-						value="spotify"
-						className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-green-800/70 hover:bg-neutral-800 hover:text-green-400 data-[state=on]:bg-neutral-900 data-[state=on]:text-green-500">
-						<SiSpotify className="h-7 w-7" />
-					</ToggleGroupItem>
-					{/* <ToggleGroupItem
-						value="tasks"
-						className="hover:bg-neutral-800 data-[state=on]:bg-neutral-900 flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3">
-						<FaTasks className=" text-ctp-sapphire h-7 w-7" />
-					</ToggleGroupItem> */}
-					<ToggleGroupItem
+		<TooltipProvider>
+			<Popover>
+				<PopoverTrigger
+					className="m-4 mr-2 w-full"
+					onClick={() => checkValues()}>
+					<div className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-950 p-4 ring-2 ring-neutral-800 transition-all duration-200 ease-in-out hover:bg-neutral-900">
+						<FiSettings className="h-10 w-10 text-stone-300" size={'4em'} />
+					</div>
+				</PopoverTrigger>
+				<PopoverContent
+					onOpenAutoFocus={(e) => e.preventDefault()}
+					className="mb-1 ml-2 flex h-full w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
+					<ToggleGroup
+						type="multiple"
+						className="space-x-2"
+						defaultValue={toggleVal}
+						onValueChange={(value) => toggleValues(value)}>
+						<ToggleGroupItem
+							value="spotify"
+							className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-green-600/70 hover:bg-neutral-900 hover:text-green-500/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-green-500 data-[state=on]:hover:bg-neutral-900 data-[state=on]:hover:text-green-600">
+							<SiSpotify className="size-7" />
+						</ToggleGroupItem>
+						<ToggleGroupItem
+							value="countdown"
+							className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-slate-600/70 hover:bg-neutral-900/60 hover:text-slate-600/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-slate-500 data-[state=on]:hover:bg-neutral-900/60 data-[state=on]:hover:text-slate-500/90">
+							<FiCalendar className="size-7" />
+						</ToggleGroupItem>
+						{/* <ToggleGroupItem
 						value="minigames"
 						className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-red-800/70 hover:bg-neutral-800 hover:text-red-400 data-[state=on]:bg-neutral-900 data-[state=on]:text-red-600 data-[state=on]:hover:bg-neutral-800 data-[state=on]:hover:text-red-500">
-						<FaGamepad className="h-7 w-7" />
-					</ToggleGroupItem>
-				</ToggleGroup>
-				<button
-					onClick={() => resetSpotifyData()}
-					className="mt-4 flex flex-col rounded-lg border-2 border-neutral-700 p-3 font-semibold text-neutral-300">
-					Reset Spotify Data{' '}
-					<span className="text-xs font-medium text-neutral-400">
-						(Use if the spotify widget isn't loading.)
-					</span>
-				</button>
-				<ApiKeyPopup />
-				{/* <span className=" text-neutral-400 pt-4 text-xs font-medium">
-					(Press the "s" key to toggle the settings button.)
-				</span> */}
-			</PopoverContent>
-		</Popover>
+						<FaGamepad className="size-7" />
+						</ToggleGroupItem> */}
+						<Tooltip>
+							<TooltipTrigger>
+								<ContextMenu>
+									<ContextMenuTrigger>
+										<ToggleGroupItem
+											value="weather"
+											className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-amber-600/70 hover:bg-neutral-900 hover:text-amber-500/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-amber-500/90 data-[state=on]:hover:bg-neutral-900 data-[state=on]:hover:text-amber-600">
+											<TiWeatherCloudy className="size-7" />
+										</ToggleGroupItem>
+									</ContextMenuTrigger>
+									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
+										<ToggleGroup
+											type="single"
+											className="space-x-[0.375rem]"
+											defaultValue={localStorage.getItem('temp') || 'f'}
+											onValueChange={(value) => toggleTemp(value)}>
+											<ToggleGroupItem
+												className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-zinc-400/70 hover:bg-neutral-900 hover:text-zinc-300/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-zinc-300 data-[state=on]:hover:bg-neutral-900 data-[state=on]:hover:text-zinc-400"
+												value="f">
+												<TbTemperatureFahrenheit className="size-5" />
+											</ToggleGroupItem>
+											<ToggleGroupItem
+												className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-zinc-400/70 hover:bg-neutral-900 hover:text-zinc-300/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-zinc-300 data-[state=on]:hover:bg-neutral-900 data-[state=on]:hover:text-zinc-400"
+												value="c">
+												<TbTemperatureCelsius className="size-5" />
+											</ToggleGroupItem>
+										</ToggleGroup>
+										<div className="flex flex-row items-center gap-x-3">
+											<TiWeatherCloudy className="size-10 text-amber-600" />
+											<Input
+												// placeholder="OpenWeatherMap API Key"
+												placeholder="Not Yet Implemented"
+												type="password"
+												className="text-md w-full rounded-lg border-neutral-700 bg-neutral-950 px-3 text-neutral-400 placeholder-stone-200 ring-1 ring-neutral-700 placeholder:font-bold focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-0"
+											/>
+										</div>
+										{/* <Toggle */}
+									</ContextMenuContent>
+								</ContextMenu>
+							</TooltipTrigger>
+							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-950 p-2">
+								<p className="font-mono text-xs font-semibold text-neutral-300">
+									Right click to edit weather settings.
+								</p>
+							</TooltipContent>
+						</Tooltip>
+					</ToggleGroup>
+
+					<button
+						onClick={() => resetSpotifyData()}
+						className="flex flex-col rounded-lg border-2 border-neutral-700 p-3 font-semibold text-neutral-300">
+						Reset Spotify Auth Data{' '}
+						<span className="text-xs font-medium text-neutral-400">
+							(Use if the spotify widget isn't loading.)
+						</span>
+					</button>
+					{/* <ApiKeyPopup /> */}
+				</PopoverContent>
+			</Popover>
+		</TooltipProvider>
 	);
 }
