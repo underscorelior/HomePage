17989c91d6cffaa7e839cde0674c8f86d17ee489
Lior Pendler <85250652+underscorelior@users.noreply.github.com>
2024-04-09 00:57:56 -0700

    Update index.tsx to be up to date.

--- src/index.tsx
+++ src/index.tsx
@@ -3,35 +3,33 @@ import './index.css';
 import Clock from './components/Clock';
 import Spotify from './components/Spotify';
 import { Toaster } from 'react-hot-toast';
-// import Tasks from './components/Tasks';
-// import Settings from './components/Settings';
-// import Minigames from './components/Minigames';
-// import { useState } from 'react';
+import Countdown from './components/Countdown';
+import Settings from './components/Settings';
+import { useState } from 'react';
 
 export function App() {
-	// const [spotify, setSpotify] = useState<boolean>(false);
-	// const [tasks, setTasks] = useState<boolean>(false);
-	// const [minigames, setMinigames] = useState<boolean>(false);
-	// const [settingsOpen, setSettingsOpen] = useState<boolean>(true);
+	const [spotify, setSpotify] = useState<boolean>(false);
+	const [countdown, setCountdown] = useState<boolean>(false);
+	const [weather, setWeather] = useState<boolean>(false);
+	const [temp, setTemp] = useState<string>(localStorage.getItem('temp') || 'c');
 
 	return (
 		<div className="flex h-screen w-screen flex-col overflow-hidden bg-neutral-950">
 			<Toaster />
-			<Clock />
+			<Clock unit={temp} weather={weather} />
 			<div className="fixed bottom-0 flex h-max w-full justify-center">
 				<div className="fixed bottom-0 left-0 flex flex-row">
-					{/* <div className="fixed bottom-0 left-0 flex flex-row">
-						setSpotify={setSpotify}
-						// setTasks={setTasks}
-						setMinigames={setMinigames}
-						setSettingsOpen={setSettingsOpen}
-						settingsOpen={settingsOpen}
-					/>
-					{minigames && <Minigames />}
-				</div> */}
-					{/* {tasks && <Tasks />} */}
+					<div className="fixed bottom-0 left-0 flex flex-row">
+						<Settings
+							setSpotify={setSpotify}
+							setCountdown={setCountdown}
+							setTemp={setTemp}
+							setWeather={setWeather}
+						/>
+					</div>
 				</div>
-				<Spotify />
+				{spotify && <Spotify />}
+				{countdown && <Countdown />}
 			</div>
 		</div>
 	);
