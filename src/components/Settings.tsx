998d6812af6518179b357194ab3bd1cc12345ff2
Lior Pendler <lior.pendler@gmail.com>
2024-06-22 18:29:04 -0700

    Partially finish settings light mode

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -33,6 +33,7 @@ import { SiSpotify } from 'react-icons/si';
 import { TbTemperatureCelsius, TbTemperatureFahrenheit } from 'react-icons/tb';
 import { TiWeatherCloudy } from 'react-icons/ti';
 import { RedirContext } from '..';
+import { DateTimePicker } from '@/shadcn/components/ui/date-time-picker/date-time-picker';
 
 export default function Settings({
 	setSpotify,
@@ -122,13 +123,16 @@ export default function Settings({
 				<PopoverTrigger
 					className="m-4 mr-2 w-full"
 					onClick={() => checkValues()}>
-					<div className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-950 p-4 ring-2 ring-neutral-800 transition-all duration-200 ease-in-out hover:bg-neutral-900">
-						<FiSettings className="h-10 w-10 text-stone-300" size={'4em'} />
+					<div className="flex h-14 w-14 items-center justify-center rounded-lg bg-neutral-50 p-4 ring-2 ring-neutral-600 transition-all duration-200 ease-in-out hover:bg-neutral-200 dark:bg-neutral-950 dark:ring-neutral-800 hover:dark:bg-neutral-900">
+						<FiSettings
+							className="h-10 w-10 text-stone-800 hover:text-stone-950 dark:text-stone-300 dark:hover:text-stone-200"
+							size={'4em'}
+						/>
 					</div>
 				</PopoverTrigger>
 				<PopoverContent
 					onOpenAutoFocus={(e) => e.preventDefault()}
-					className="mb-1 ml-2 flex h-full w-full flex-col gap-y-4 rounded-lg border-[2px] border-neutral-800 bg-neutral-950 p-4">
+					className="mb-1 ml-2 flex h-full w-full flex-col gap-y-4 rounded-lg border-[2px] border-neutral-800 bg-neutral-50 p-4 dark:bg-neutral-950">
 					<ToggleGroup
 						type="multiple"
 						className="space-x-2"
@@ -140,11 +144,11 @@ export default function Settings({
 									<ContextMenuTrigger>
 										<ToggleGroupItem
 											value="spotify"
-											className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-green-600/70 hover:bg-neutral-900 hover:text-green-500/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-green-500 data-[state=on]:hover:bg-neutral-900 data-[state=on]:hover:text-green-600">
+											className="flex aspect-square size-auto rounded-lg border-2 border-neutral-500 p-3 text-green-500/60 hover:bg-neutral-100/80 hover:text-green-500/90 data-[state=on]:bg-neutral-100 data-[state=on]:text-green-500 hover:data-[state=on]:bg-neutral-100/70 hover:data-[state=on]:text-green-500/80 dark:border-neutral-700 dark:bg-neutral-950 dark:hover:bg-neutral-900/80 dark:hover:text-green-500/90 dark:data-[state=on]:bg-neutral-900 dark:data-[state=on]:text-green-500 dark:hover:data-[state=on]:bg-neutral-900/80 dark:data-[state=on]:hover:text-green-500/70">
 											<SiSpotify className="size-7" />
 										</ToggleGroupItem>
 									</ContextMenuTrigger>
-									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
+									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-50 p-4 dark:bg-neutral-950">
 										<div className="flex flex-row items-center gap-x-3">
 											<SiSpotify className="size-10 text-green-400" />
 											<Input
@@ -152,12 +156,12 @@ export default function Settings({
 												disabled
 												placeholder="Not Yet Implemented"
 												type="password"
-												className="w-full rounded-lg border-neutral-700 bg-neutral-950 px-3 text-lg text-neutral-400 placeholder-stone-200 outline-none ring-2 ring-neutral-700 placeholder:font-bold focus:outline-stone-800 active:outline-stone-800"
+												className="w-full rounded-lg border-neutral-700 bg-neutral-50 px-3 text-lg text-neutral-400 placeholder-stone-200 outline-none ring-2 ring-neutral-700 placeholder:font-bold focus:outline-stone-800 active:outline-stone-800 dark:bg-neutral-950"
 											/>
 										</div>
 										<button
 											onClick={() => resetSpotifyData()}
-											className="mx-auto flex w-auto flex-col rounded-lg border-2 border-neutral-700 p-3 text-center text-sm font-semibold text-neutral-300">
+											className="mx-auto flex w-auto flex-col rounded-lg border-2 border-neutral-700 p-3 text-center text-sm font-semibold text-neutral-800 dark:text-neutral-300">
 											<p className="mx-auto w-full text-center">
 												Reset Spotify Auth Data
 											</p>
@@ -168,8 +172,8 @@ export default function Settings({
 									</ContextMenuContent>
 								</ContextMenu>
 							</TooltipTrigger>
-							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-950 p-2">
-								<p className="font-mono text-xs font-semibold text-neutral-300">
+							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-50 p-2 dark:bg-neutral-950">
+								<p className="font-mono text-xs font-semibold text-neutral-800 dark:text-neutral-300">
 									Right click to edit Spotify settings.
 								</p>
 							</TooltipContent>
@@ -180,17 +184,21 @@ export default function Settings({
 									<ContextMenuTrigger>
 										<ToggleGroupItem
 											value="countdown"
-											className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-slate-600/70 hover:bg-neutral-900/60 hover:text-slate-600/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-slate-500 data-[state=on]:hover:bg-neutral-900/60 data-[state=on]:hover:text-slate-500/90">
+											className="flex aspect-square size-auto rounded-lg border-2 border-neutral-500 p-3 text-slate-500/60 hover:bg-neutral-100/80 hover:text-slate-500/90 data-[state=on]:bg-neutral-100 data-[state=on]:text-slate-500 hover:data-[state=on]:bg-neutral-100/70 hover:data-[state=on]:text-slate-500/80 dark:border-neutral-700 dark:bg-neutral-950 dark:hover:bg-neutral-900/80 dark:hover:text-slate-500/90 dark:data-[state=on]:bg-neutral-900 dark:data-[state=on]:text-slate-500 dark:hover:data-[state=on]:bg-neutral-900/80 dark:data-[state=on]:hover:text-slate-500/70">
 											<FiCalendar className="size-7" />
 										</ToggleGroupItem>
 									</ContextMenuTrigger>
-									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
+									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-50 p-4 dark:bg-neutral-950">
 										{/* Change timestamp for 2 different days + rename */}
+										{/* <DateTimePicker date={date} setDate={setDate} /> */}
+										<DateTimePicker granularity={'minute'} />
+										{/*TODO: Just make a custom elementwith 6 input */}
+										{/*Store stuff in the format of name, timestamp. Max of 3-4, max length of name */}
 									</ContextMenuContent>
 								</ContextMenu>
 							</TooltipTrigger>
-							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-950 p-2">
-								<p className="font-mono text-xs font-semibold text-neutral-300">
+							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-50 p-2 dark:bg-neutral-950">
+								<p className="font-mono text-xs font-semibold text-neutral-800 dark:text-neutral-300">
 									Right click to edit countdown settings.
 								</p>
 							</TooltipContent>
@@ -201,11 +209,11 @@ export default function Settings({
 									<ContextMenuTrigger>
 										<ToggleGroupItem
 											value="weather"
-											className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-amber-600/70 hover:bg-neutral-900 hover:text-amber-500/90 data-[state=on]:bg-neutral-900 data-[state=on]:text-amber-500/90 data-[state=on]:hover:bg-neutral-900 data-[state=on]:hover:text-amber-600">
+											className="flex aspect-square size-auto rounded-lg border-2 border-neutral-500 p-3 text-amber-600/60 hover:bg-neutral-100/80 hover:text-amber-600/90 data-[state=on]:bg-neutral-100 data-[state=on]:text-amber-600 hover:data-[state=on]:bg-neutral-100/70 hover:data-[state=on]:text-amber-600/80 dark:border-neutral-700 dark:bg-neutral-950 dark:hover:bg-neutral-900/80 dark:hover:text-amber-600/90 dark:data-[state=on]:bg-neutral-900 dark:data-[state=on]:text-amber-600 dark:hover:data-[state=on]:bg-neutral-900/80 dark:data-[state=on]:hover:text-amber-600/70">
 											<TiWeatherCloudy className="size-7" />
 										</ToggleGroupItem>
 									</ContextMenuTrigger>
-									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-950 p-4">
+									<ContextMenuContent className="mb-1 ml-2 flex w-full flex-col gap-y-4 rounded-lg border-[3px] border-neutral-700 bg-neutral-50 p-4 dark:bg-neutral-950">
 										<ToggleGroup
 											type="single"
 											className="space-x-[0.375rem]"
@@ -236,14 +244,14 @@ export default function Settings({
 												disabled
 												placeholder="Not Yet Implemented"
 												type="password"
-												className="text-md w-full rounded-lg border-neutral-700 bg-neutral-950 px-3 text-neutral-400 placeholder-stone-200 ring-1 ring-neutral-700 placeholder:font-bold focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-0"
+												className="text-md w-full rounded-lg border-neutral-700 bg-neutral-50 px-3 text-neutral-400 placeholder-stone-200 ring-1 ring-neutral-700 placeholder:font-bold focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-0 dark:bg-neutral-950"
 											/>
 										</div>
 									</ContextMenuContent>
 								</ContextMenu>
 							</TooltipTrigger>
-							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-950 p-2">
-								<p className="font-mono text-xs font-semibold text-neutral-300">
+							<TooltipContent className="m-1 border-2 border-neutral-700 bg-neutral-50 p-2 dark:bg-neutral-950">
+								<p className="font-mono text-xs font-semibold text-neutral-800 dark:text-neutral-300">
 									Right click to edit weather settings.
 								</p>
 							</TooltipContent>
@@ -251,7 +259,7 @@ export default function Settings({
 
 						{/* <ToggleGroupItem
 						value="minigames"
-						className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-red-800/70 hover:bg-neutral-800 hover:text-red-400 data-[state=on]:bg-neutral-900 data-[state=on]:text-red-600 data-[state=on]:hover:bg-neutral-800 data-[state=on]:hover:text-red-500">
+						className="flex aspect-square h-auto w-auto items-center justify-center rounded-lg border-2 border-neutral-700 p-3 text-red-800/70 hover:bg-neutral-200 dark:bg-neutral-800 hover:text-red-400 data-[state=on]:bg-neutral-900 data-[state=on]:text-red-600 data-[state=on]:hover:bg-neutral-200 dark:bg-neutral-800 data-[state=on]:hover:text-red-500">
 						<FaGamepad className="size-7" />
 						</ToggleGroupItem> */}
 					</ToggleGroup>
