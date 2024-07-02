9f6cb6a4f7d0d65130e8da6d734dd66235005e69
Lior Pendler <lior.pendler@gmail.com>
2024-07-01 22:42:27 -0800

    Create sync helper methods

--- src/utils/sync.ts
+++ src/utils/sync.ts
@@ -1,4 +1,18 @@
-export function handleCreate() {}
+// TODO: Allow for customization of backend URL
+
+export async function handleCreate(): Promise<string> {
+	const res = await fetch(
+		'https://homepage-backend-seven.vercel.app/api/sync/create',
+		{
+			method: 'GET',
+		},
+	);
+
+	const out = await res.json();
+
+	if (res.status == 200) return out.code;
+	else return 'An error has occurred' + out.message;
+}
 
 export function handleUpdate() {}
 
